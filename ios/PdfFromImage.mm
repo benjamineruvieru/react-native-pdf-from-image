dcff3d40b90ca6e3938c41d1940e6a7a3f589df9
Benjamin <benjamineruvieru@gmail.com>
2025-01-31 12:00:28 +0100

    fix: fixes createPdf method for legacy architecture support on ios

--- ios/PdfFromImage.mm
+++ ios/PdfFromImage.mm
@@ -4,6 +4,7 @@
 @implementation PdfFromImage
 RCT_EXPORT_MODULE()
 
+#ifdef RCT_NEW_ARCH_ENABLED
 - (NSDictionary *)createPdf:
     (JS::NativePdfFromImage::ImageObjectNative &)imageObject {
 
@@ -74,6 +75,73 @@ RCT_EXPORT_MODULE()
   NSLog(@"PDF created at path: %@", tempPath);
   return @{@"filePath" : tempPath};
 }
+#else
+RCT_EXPORT_METHOD(createPdf : (NSDictionary *)imageObject resolver : (
+    RCTPromiseResolveBlock)resolve rejecter : (RCTPromiseRejectBlock)reject) {
+
+  NSLog(@"Starting PDF creation");
+    
+  NSString *documentName = [imageObject objectForKey:@"name"] ?: @"document";
+  NSArray *imagePaths = [imageObject objectForKey:@"imagePaths"];
+  NSDictionary *paperSizeDict = [imageObject objectForKey:@"paperSize"];
+
+  CGSize paperSize = CGSizeZero;
+  if (paperSizeDict) {
+    paperSize = CGSizeMake([[paperSizeDict objectForKey:@"width"] floatValue],
+                           [[paperSizeDict objectForKey:@"height"] floatValue]);
+  }
+
+  NSLog(@"Document name: %@", documentName);
+  NSLog(@"Image paths: %@", imagePaths);
+  NSLog(@"Paper size: Width = %.2f, Height = %.2f", paperSize.width,
+        paperSize.height);
+
+  // Create PDF context
+  NSString *tempPath = [NSTemporaryDirectory()
+      stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.pdf",
+                                                                documentName]];
+  UIGraphicsBeginPDFContextToFile(tempPath, CGRectZero, nil);
+
+  for (NSString *imagePath in imagePaths) {
+    UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
+    if (image) {
+
+      NSLog(@"Successfully loaded image from path: %@", imagePath);
+      // Begin new PDF page with the size of the image
+      CGRect pageRect;
+      if (!CGSizeEqualToSize(paperSize, CGSizeZero)) {
+        pageRect = CGRectMake(0, 0, paperSize.width, paperSize.height);
+        // Calculate scaling factors
+        CGFloat widthRatio = paperSize.width / image.size.width;
+        CGFloat heightRatio = paperSize.height / image.size.height;
+        CGFloat scale = MIN(widthRatio, heightRatio);
+
+        CGFloat scaledWidth = image.size.width * scale;
+        CGFloat scaledHeight = image.size.height * scale;
+        CGFloat xOffset = (paperSize.width - scaledWidth) / 2;
+        CGFloat yOffset = (paperSize.height - scaledHeight) / 2;
+
+        UIGraphicsBeginPDFPageWithInfo(pageRect, nil);
+        CGRect imageRect =
+            CGRectMake(xOffset, yOffset, scaledWidth, scaledHeight);
+        [image drawInRect:imageRect];
+      } else {
+        pageRect = CGRectMake(0, 0, image.size.width, image.size.height);
+        UIGraphicsBeginPDFPageWithInfo(pageRect, nil);
+        [image drawInRect:pageRect];
+      }
+
+    } else {
+      NSLog(@"Failed to load image from path: %@", imagePath);
+      reject(@"event_failure", @"Failed to load image from path:", nil);
+    }
+  }
+
+  UIGraphicsEndPDFContext();
+  NSLog(@"PDF created at path: %@", tempPath);
+  resolve(@{@"filePath" : tempPath});
+}
+#endif
 
 #ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
